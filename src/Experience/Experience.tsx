1da45be70768d80522d4e9c23c6f84b608c65acf
rmit-s3722599-Minh-Le <minhle46@gmail.com>
2025-02-18 00:29:23 +1100

    chores: update resume description

--- src/Experience/Experience.tsx
+++ src/Experience/Experience.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './Experience.css';
 
 function Experience() {
@@ -13,9 +12,7 @@ function Experience() {
     } 
     return (
     <div className="Homepage">
-      {/* This is the page content. This will used as the template */}
       <div className="Experience">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <h1>
           My experiences
         </h1>
@@ -42,12 +39,14 @@ function Experience() {
             2020-2023 - FULL TIME JUNIOR Frontend Developer 
           </h3>
           <p>
-            Built a portal (https://nabhive.nab.com.au/login was the prod version of the portal) for merchant customers.  
+            Helped develop a portal (https://nabhive.nab.com.au/login was the prod version of the portal) for merchant customers.  
           </p>
           <ul>
-              {listPoint("Developed 70% of the front-end login page, including the hamburger menu and the portal.")}
-              {listPoint("Was highlighted for my contributions toward the front-end login page by the microservice team.")}
-              {listPoint("Acknowledged by the UX/UI and the back-end team for continuing to ensure the required features were developed as intended.")}
+              {listPoint(`Delivery of features (Using React/Typescript, CSS, Javscript, HTML and Node.JS) with constant 
+                communication with UI/UX team, Backend team and other Front-end teams.`)}
+              {listPoint("Implemented mobile/tablet responsiveness to web pages and features like menu bar becoming hamburger menu through breakpoints.")}
+              {listPoint("Experienced with unit testing with Jest/React Testing Library, providing coverage to code.")}
+              {listPoint("CI/CD, frequently deploying to non-production/production using Jenkins/Harness pipeline.")}
               {listPoint("Monitored and frequently updated the packages and the portal, ensuring potential minimal issues.")}
               {listPoint("Taught co-workers the necessary steps with documentations in order to develop, update and troubleshoot the front-end code.")}
               {listPoint("Immediately identified and fixed defects, ensuring seamless usability for merchants.")}
